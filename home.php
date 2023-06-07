76f5b2ae936a88a74c120639ab7df0a038cbb14e
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-06-07 23:35:52 +0700

    modified

--- home.php
+++ home.php
@@ -1,29 +1,41 @@
  <!--css tailwind-->
  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 <article>
-  <div id="form"  class="auto-screen flex flex-col items-center justify-center">
-    <form method="post" action="" class="mt-8 mb-2 w-50 max-w-screen-lg sm:w-80">
-      <div class="mb-8 flex flex-col gap-2">
-        <div class="relative h-11 w-full min-w-[150px]">
+  <div id="form" style="text-align:center">
+    <form method="post" action="" className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+      <div className="mb-4 flex flex-col gap-6">
+      <div className="relative h-11 w-full min-w-[200px]">
       <p>Masukkan Kata :</p>
-      <input type="text" name="kata" id="kata" size="100" class="flex-2 mr-4 p-3 border border-white-600 text-grey rounded" placeholder="contoh: menyanyikan"
+      <input type="text" name="kata" id="kata" size="100" class="flex-1 mr-2 p-2 border border-white-400 text-grey rounded" placeholder="contoh: menyanyikan"
         value="<?php if(isset($_POST['kata'])){ echo $_POST['kata']; }else{ echo '';}?>">
       <br />
       <button type="submit" name="submit" class="py-2 px-4 bg-blue-500 text-white rounded hover:bg-green-500 middle none center rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-grey-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
       data-ripple-light="true">Mencari</button>
-        </div>
-    </div>
     </form>
     <br />
- 
+
     <?php 
+    $teksAsli = '';
+    $striming = '';
+
     if(isset($_POST['kata'])){
       $teksAsli = $_POST['kata'];
-      echo "<p>teks asli : " .$teksAsli. "</p>";
       $striming = striming($teksAsli);
-      echo "<p>kata dasar : <b>" .$striming. "</b></p>";
-      }
-    ?>
+    ?> 
+
+    <div class="w-50 max-w-screen-lg sm:w-80 bg-white rounded-lg shadow-md p-8">
+      <h2 class="text-xl font-bold mb-4">Hasil Pencarian</h2>
+      <div class="mb-4">
+        <p class="font-semibold">Teks Asli:</p>
+        <p><?php echo $teksAsli; ?></p>
+      </div>
+      <div>
+        <p class="font-semibold">Kata Dasar:</p>
+        <p><?php echo $striming; ?></p>
+      </div>
+    </div>
+
+    <?php } ?>
 
   </div>
 </article>
